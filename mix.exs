9a04f40c99ef71161774e507afd5b2544ae8881b
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-14 21:56:27 +0100

    mix lock

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Workdays.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
@@ -35,7 +36,7 @@ defmodule Workdays.MixProject do
 
   defp package() do
     [
-      name: "calendar-workdays",
+      name: "calendar_workdays",
       files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/patrykwozinski/workdays"}
