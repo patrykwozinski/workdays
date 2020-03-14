8d27c09aa594984b8985e22a02291de8c83f7457
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-14 17:54:39 +0100

    init

---
+++ lib/workdays.ex
@@ -0,0 +1,18 @@
+defmodule Workdays do
+  @moduledoc """
+  Documentation for Workdays.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Workdays.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/workdays_test.exs
@@ -0,0 +1,8 @@
+defmodule WorkdaysTest do
+  use ExUnit.Case
+  doctest Workdays
+
+  test "greets the world" do
+    assert Workdays.hello() == :world
+  end
+end
