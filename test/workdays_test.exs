b43cb041f6b0b52e07de41822726d732f04d013c
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-14 18:11:53 +0100

    test to cver saturday

--- test/workdays_test.exs
+++ test/workdays_test.exs
@@ -5,4 +5,8 @@ defmodule WorkdaysTest do
   test "9th of March is work day" do
     assert Workdays.workday?("2020-03-09") == true
   end
+
+  test "14th of March which is Saturday is not a work day" do
+    assert Workdays.workday?("2020-03-14") == false
+  end
 end
