d459b574d506cf0311887d3e9bfeba3d20d7b277
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-14 22:23:36 +0100

    app name changed

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Workdays.MixProject do
 
   def project do
     [
-      app: :workdays,
+      app: :calendar_workdays,
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
