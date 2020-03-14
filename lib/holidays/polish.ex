b701668dd13d0621187f31edc5185238d2fbfee7
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-14 21:23:16 +0100

    mooore

--- lib/holidays/polish.ex
+++ lib/holidays/polish.ex
@@ -4,8 +4,12 @@ defmodule Calendar.Holidays.Polish do
   @doc false
   def bank_holiday?(date) do
     cond do
+      date.month == 1 and date.day == 6 -> true
       date.month == 5 and date.day == 1 -> true
       date.month == 5 and date.day == 3 -> true
+      date.month == 8 and date.day == 15 -> true
+      date.month == 11 and date.day == 1 -> true
+      date.month == 11 and date.day == 11 -> true
       true -> false
     end
   end
--- test/calendar_test.exs
+++ test/calendar_test.exs
@@ -22,7 +22,11 @@ defmodule CalendarTest do
   end
 
   test "Polish bank holidays are not a work days" do
+    assert Calendar.workday?(~D[2020-01-06]) == false
     assert Calendar.workday?(~D[2020-05-01]) == false
     assert Calendar.workday?(~D[2020-05-03]) == false
+    assert Calendar.workday?(~D[2020-08-15]) == false
+    assert Calendar.workday?(~D[2020-11-01]) == false
+    assert Calendar.workday?(~D[2020-11-11]) == false
   end
 end
